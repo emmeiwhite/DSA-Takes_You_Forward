b1948505141e0a7d3e3586af48d5bb470d02c11a
emmeiwhite <emmeiwhite@gmail.com>
2024-11-11 08:52:22 +0530

    dsa- 1.3 numeric patterns - right angled triangles --- also bugs fixed

--- 1.0-basic-patterns.js
+++ 1.0-basic-patterns.js
@@ -22,8 +22,10 @@ export default function pattern(n) {
  *  * *
  *  * * *
  *  * * * *
+ *  * * * * *
  */
 
+/** Solution 2
 export default function pattern(n) {
   for (let i = 0; i < n; i++) {
     let starsInLine = ''
@@ -33,3 +35,44 @@ export default function pattern(n) {
     console.log(`${starsInLine}`)
   }
 }
+ */
+
+/** Patter-3
+ *  1
+ *  1 2
+ *  1 2 3
+ *  1 2 3 4
+ *  1 2 3 4 5
+ */
+
+/** --- Solution-3
+export default function pattern(n) {
+  for (let i = 0; i < n; i++) {
+    let numbersLine = ''
+    for (let j = 0; j <= i; j++) {
+      numbersLine += `${j + 1} `
+    }
+    console.log(`${numbersLine}`)
+  }
+}
+--- */
+
+/** Patter-4
+ *  1
+ *  2 2
+ *  3 3 3
+ *  4 4 4 4
+ *  5 5 5 5 5
+ */
+
+/** Solution-4
+export default function pattern(n) {
+  for (let i = 0; i < n; i++) {
+    let numbersLine = ''
+    for (let j = i; j <= i; j++) {
+      numbersLine += `${j + 1} `
+    }
+    console.log(`${numbersLine}`)
+  }
+}
+--- */
