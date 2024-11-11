1b8b4b0ceacaf14ac016da2a7ddcf9cc84ddc5a1
emmeiwhite <emmeiwhite@gmail.com>
2024-11-11 10:09:27 +0530

    dsa-2.1 upside down pyramid with just 2 sub-loops

--- 2.0-intermediate-patterns.js
+++ 2.0-intermediate-patterns.js
@@ -7,23 +7,56 @@
  *
  */
 
+/*
 export default function intermediatePatterns(n) {
   for (let i = 0; i < n; i++) {
-    /** a)---  Print Spaces --- */
     let row = ''
+
+     // Print Spaces 
     for (let j = 0; j < n - i - 1; j++) {
       row += ' '
     }
 
-    /** b)---  Print stars --- */
+   // Print stars 
     for (let j = 0; j <= 2 * i; j++) {
       row += '*'
     }
 
-    /** c)---  Print Spaces --- */
+    //  Print Spaces 
     for (let j = 0; j < n - i - 1; j++) {
       row += ' '
     }
+    console.log(row)
+  }
+}
+*/
+
+/** Pattern-2
+ * *********
+ *  *******
+ *   *****
+ *    ***
+ *     *
+ */
+
+export default function intermediatePatterns(n) {
+  for (let i = 0; i < n; i++) {
+    // Print Spaces
+    let row = ''
+    for (let j = 0; j < i; j++) {
+      row += ' '
+    }
+
+    // Print stars
+    for (let j = i; j < 2 * n - i - 1; j++) {
+      // Here we start j from i for each i
+      row += '*'
+    }
+
+    //  Print Spaces This is not required
+    //     for (let j = 0; j < i; j++) {
+    //       row += ' '
+    //     }
     console.log(row)
   }
 }
