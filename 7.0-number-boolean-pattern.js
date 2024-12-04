c795dd40b1f69b47887e924cb9561ca88de83e18
emmeiwhite <emmeiwhite@gmail.com>
2024-12-04 10:49:22 +0530

    dsa-6.4 most optimized solution and trick with 1-start for binary pattern

--- 7.0-number-boolean-pattern.js
+++ 7.0-number-boolean-pattern.js
@@ -40,13 +40,34 @@ export default function intermediatePatterns(n) {
 
 --- */
 
+/**  ---
 export default function intermediatePatterns(n) {
   for (let i = 0; i < n; i++) {
     let line = ''
 
     // Just a one liner optimal solution but it needs mind twisting and thinking to improve logic
     for (let j = 0; j <= i; j++) {
-      line += (i + j) % 2 === 0 ? '1' : '0'
+      line += (i + j) % 2 === 0 ? ' 1 ' : ' 0 '
+    }
+    console.log(line)
+  }
+}
+
+--- */
+
+/** SOLUTION-3 with Maths trick since 1-1=0 & this is very helpful in our pattern  */
+
+export default function intermediatePatterns(n) {
+  let start
+  for (let i = 0; i < n; i++) {
+    let line = ''
+
+    // We know all the even lines start with 1
+    if (i % 2 === 0) start = 1
+
+    for (let j = 0; j <= i; j++) {
+      line += String(start)
+      start = 1 - start
     }
     console.log(line)
   }
